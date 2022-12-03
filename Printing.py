3189d54bb635e65fdb6f06d992895626133ea880
Panda-Coil <k.harris.osaka@gmail.com>
2022-12-03 18:00:14 +0900

    22Dec3No2

--- Printing.py
+++ Printing.py
@@ -3,31 +3,34 @@
 
 import fpdf
 import os
-import time
 import subprocess
-import shlex
+
+
 
 #gets the original directory
 origWD = os.getcwd()
 
 #directories
 folderpath = r"/Users/kane/Desktop/work/tax/22_gensen_pdf/"
-program = r"/System/Applications/Preview.app"
 
 #changes directory to the file
 os.chdir(folderpath)
 
 #makes a list for all files
-files = [name for name in os.listdir(folderpath)]
+files = ["lp " + name for name in os.listdir(folderpath)]
 print(files)
-      
+    
+
 #prints each file
 for name in files:
-    #subprocess.call(('open', name))
-    subprocess.call("lp -p", name)
+    os.system(name)
+
+
+
+
+#os.startfile(name, "print") windows only
     
-    
-    #os.startfile(name, "print")
-    #os.open(name, 'print')
+#subprocess.call(('open', name)) works
+#os.open(name, 'print')
 
 
